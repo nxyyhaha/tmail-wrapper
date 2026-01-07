4a20fa2f2dc40a4a250953e87e4268a92a802917
nxyy <175990828+nxyystore@users.noreply.github.com>
2026-01-06 23:08:35 -0500

    g

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='tmail-py',
-	version='0.1.0',
+	version='0.0.2',
 	description='Python wrapper for TMail API',
 	packages=find_packages(exclude=("tests",)),
 	install_requires=[
